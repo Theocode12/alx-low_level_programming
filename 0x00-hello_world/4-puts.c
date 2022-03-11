7329719b6ff2d5cd345a0fb9f2fc4a9db5ab5765
root <vchidubem9@gmail.com>
2022-03-10 16:02:04 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- *Return: Always 0 (Success)
- * /
+ * Return: Always 0 (Success)
+ */
  int main(void)
  {
  	puts("\"programming is like building a multilingual puzzel");
