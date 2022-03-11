32f5369b3f98ad5c9e4ca05603408fd46d9c5ffe
root <vchidubem9@gmail.com>
2022-03-10 16:56:04 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char mystr[]="\"programming is like building a multilingual puzzle";
-	puts(mystr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
