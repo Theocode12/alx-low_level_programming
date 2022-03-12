0b3335c36a90712c60a265d5b645471b5886ed4d
root <vchidubem9@gmail.com>
2022-03-11 17:07:46 -0800

    ascii

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i;
 
-	i=97;
-	while(i <= 122)
+	i = 97;
+	while (i <= 122)
 	{
 		putchar(i);
 		i++;
 	}
-	i=65;
-	while(i <= 90)
+	i = 65;
+	while (i <= 90)
 	{
 		putchar(i);
 		i++;
