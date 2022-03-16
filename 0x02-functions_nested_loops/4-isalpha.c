5d8e676d566e5ea060f3af5a4ae9cd694482cbb9
root <vchidubem9@gmail.com>
2022-03-16 01:30:38 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a char is an alphabet
+ * @c : int parameter
+ * Return : (isalpha)
+ */
+int _isalpha(int c)
+{
+	int isalpha
+
+	isalpha = 0;
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		isalpha = 1;
+	}
+	return (isalpha);
+}
+
+
