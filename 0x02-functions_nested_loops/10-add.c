62738c4a83e3880b0f453f8924e926d4b9d4ad88
root <vchidubem9@gmail.com>
2022-03-16 08:04:33 -0700

    sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,11 @@
 
  /**
   * add - Entry point for two numbers i and j
-  *
-  * @i: parameter to be taken
-  * @j: parameter to b taken
-  * Return: the sum of i and j
+  *@i: parameter to be taken
+  *@j: parameter to b taken
+  * Return: the sum of i and j.
   */
 int add(int i, int j)
 {
-	int sum;
-
-	sum = i + j;
-	return (sum);
+	return (i + j);
 }
