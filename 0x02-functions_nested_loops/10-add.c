ae4c7ce63d3506f844460ac5d76f9db8545d1344
root <vchidubem9@gmail.com>
2022-03-16 07:52:51 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+ /**
+  * add - sums numbers
+  * @i: parameter to be taken
+  * @j: parameter to b taken
+  * Return: the sum of i and j
+  */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return sum;
+}
