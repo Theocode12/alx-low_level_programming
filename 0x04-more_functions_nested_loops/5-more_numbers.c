fdd4a058eb761ff7bf53bbcdf07ebe2b25b0e9f4
root <vchidubem9@gmail.com>
2022-03-17 20:20:38 -0700

    1-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,12 +3,13 @@
 /**
  * more_numbers - prints 10x numbers from 1 - 14
  */
+
 void more_numbers(void)
 {
 	int i, j;
 
 	j = 0;
-	while (j <= 10)
+	while (j < 10)
 	{
 		for (i = 0; i <= 14; i++)
 		{
