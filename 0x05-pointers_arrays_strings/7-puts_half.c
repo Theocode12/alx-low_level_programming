a69fdcf4f4497a30899c28fe1a6f40ebdd1a33f8
root <vchidubem9@gmail.com>
2022-03-22 17:02:48 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - take in characters and returns half of them
+ * @str: characters taken by the function
+ */
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	for (i = 0; *(str + i); i++);
+	k = i;
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+	k--;
+	for (; j <= k; )
+	{
+		_putchar(*(str + j));
+		j++;
+	}
+	putchar('\n');
+}
