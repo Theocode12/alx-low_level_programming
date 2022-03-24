0cafa1ed21d291853797f22cc4846aa96b6128f7
root <vchidubem9@gmail.com>
2022-03-24 07:53:53 -0700

    compares string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares string
+ * @s1 - string 1
+ * @s2 - string 2
+ * Return: returns 0 if equal, positive integer value if 
+ * s1 is greater else negative
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, n;
+
+	j = 0;
+	i = 0;
+	n = 0;
+	while ((*(s1 + i) || *(s2 + i)) & (j < 1))
+	{
+		if (*(s1 + i) == *(s2 + i))
+			;
+		if (*(s1 + i) > *(s2 + i))
+		{
+			n = *(s1 + i) - *(s2 + i);
+			j++;
+		}
+		if (*(s1 + i) < *(s2 + i))
+		{
+			n = *(s1 + i) - *(s2 + i);
+			j++;
+		}
+		i++;
+	}
+	return (n);
+}
