b40cbef66061fa7a2dd8ecdad097b192c019886f
root <vchidubem9@gmail.com>
2022-03-24 05:47:46 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _srncpy - copies a string to a particular number of bytes
+ * @dest: string 1
+ * @src: string 2
+ * Return: copies string 2 into string 1
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
+
