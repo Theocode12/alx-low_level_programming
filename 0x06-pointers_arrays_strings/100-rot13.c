629c3345b8b15a630f1495df99af7ceb96ef2f14
root <vchidubem9@gmail.com>
2022-03-24 17:10:51 -0700

    rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,9 +12,13 @@ char *rot13(char *str)
 	while (str[i])
 	{
 		if ((str[i] > 64 && str[i] < 78) || (str[i] > 96 && str[i] < 110))
+		{
 			*(str + i) += 13;
+		}
 		else if ((str[i] > 77 && str[i] < 91) || (str[i] > 109 && str[i] < 123))
+		{
 			*(str + i) -= 13;
+		}
 		i++;
 	}
 	return (str);
