9edf7c946f1f56f0d5c3c2617c84af68c9b814fa
root <vchidubem9@gmail.com>
2022-03-29 19:25:49 -0700

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-har *_memset(char *s, char b, unsigned intn);
+char *_memset(char *s, char b, unsigned intn);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 int _putchar(char c);
