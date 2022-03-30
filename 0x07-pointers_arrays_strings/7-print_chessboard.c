4cdcb079525a30308f42a9a69072e1e4b1ee952a
root <vchidubem9@gmail.com>
2022-03-29 20:14:36 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,10 +10,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		printf("\n");
+		_putchar('\n');
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", a[i][j]);
+			_putchar(a[i][j]);
 		}
 	}
 }
