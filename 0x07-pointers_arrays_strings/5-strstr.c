fb23f090101a49ad590af3904723a932efbc95cd
root <vchidubem9@gmail.com>
2022-03-29 19:16:51 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *src = haystack;
+		char *sub = needle;
+
+		while (*haystack && *sub && *haystack == *sub)
+		{
+			haystack++;
+			sub++;
+		}
+		if (!*sub)
+			return (src);
+		haystack = src + 1;
+	}
+	return (0);
+}
