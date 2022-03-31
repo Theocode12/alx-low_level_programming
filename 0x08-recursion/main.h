496f99ef8309104076da4d7685d03946655ec0da
root <vchidubem9@gmail.com>
2022-03-30 22:01:17 -0700

    wildcards

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,5 @@ int _putchar(int c);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
 int wildcmp(char *s1, char *s2);
-int *wildcard(char *s3, char *s4);
+char *wildcard(char *s3, char *s4);
 #endif
