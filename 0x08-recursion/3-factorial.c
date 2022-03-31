e921725f86f77ad256380d36a1eeaf0fd6c044b9
root <vchidubem9@gmail.com>
2022-03-30 19:41:04 -0700

    factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - gets the factorial of a number
+ * @n: the number
+ * Return: thr factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1);)
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+A programming technique in which a function call itself
