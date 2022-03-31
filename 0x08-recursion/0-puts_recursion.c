1d029048a5be37e5fafff32fbf9e85189e879569
root <vchidubem9@gmail.com>
2022-03-30 20:25:19 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
-	putchar(s[0]);
+	_putchar(s[0]);
 	return (_puts_recursion(s + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,4 +3,5 @@
 
 int factorial(int n);
 void _puts_recursion(char *s);
+int _putchar(int c);
 #endif
