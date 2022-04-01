422cfd3ea50ab599546ae544e81e8557b11d31db
root <vchidubem9@gmail.com>
2022-04-01 01:23:39 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,14 @@
 int prime_found(int i, int j)
 {
 	if (j != 1)
+	{
 		if (i == j)
 			return (1);
 		else if (j % i == 0 || j < 1)
 			return (0);
 		else
 			return (prime_found(i + 1, j));
+	}
 	return (0);
 }
 
