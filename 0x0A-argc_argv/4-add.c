35cc37450074cae1e95fd9860bee614010ee2613
root <vchidubem9@gmail.com>
2022-04-02 00:23:19 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,17 +7,15 @@
  * @argv: list the number of arguements
  * Return: returns 0 if succesful
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc == 1)
-		putchar('\0');
-	for (i = 0; argv[i]; i++)
+	for (i = 1; argv[i]; i++)
 	{
 		if (!(**(argv + i) >= '0' && (**(argv + i) <= '9')))
 		{
-			printf("Error\n");
+			puts("Error");
 			return (1);
 		}
 		else
