d7faea764e51772ddd55aeef7fa48c9906cd4fbb
root <vchidubem9@gmail.com>
2022-04-01 21:58:24 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of args
+ * @argv: list of args
+ * Return: multiplys args
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	c = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", c);
+	return (c);
+}
