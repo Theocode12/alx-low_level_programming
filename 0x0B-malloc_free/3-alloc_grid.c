7125a1e0056bd12530123f3097204de7f49ae0cd
root <vchidubem9@gmail.com>
2022-04-05 09:26:03 -0700

    multi-arrays with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **ptr = malloc(height * sizeof(int));
+	int **ptr = malloc(height * sizeof(int*));
 
 	if (width < 0 || height < 0)
 		return (NULL);
