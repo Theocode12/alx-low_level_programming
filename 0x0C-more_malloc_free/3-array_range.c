8f266dd8efedc6ded13f5cd52a6a9cae5521211f
root <vchidubem9@gmail.com>
2022-04-07 10:44:22 -0700

    print consecutive numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_range - array craeted to contain all values feom main to max
+ * @min: min value
+ * @max: max value
+ * Return: the pointer to new created array
+ */
+
+int *array_range(int min, int max)
+{
+	int i, size;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	ptr = malloc(sizeof(int) * size);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
