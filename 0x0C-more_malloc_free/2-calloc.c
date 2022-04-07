6d2100464d80c612aeec2082429d554aee57df66
root <vchidubem9@gmail.com>
2022-04-07 10:24:03 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,8 +11,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	char *ptr;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*(ptr + i) = 0;
 	return ((void *)ptr);
 }
