76dd57bc2604a59725f1dd5f4748b873485e7c55
root <vchidubem9@gmail.com>
2022-04-13 15:57:46 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, index;
 
-	if ((size <= 0) && (cmp != NULL) && (array != NULL))
+	if ((size > 0) && (cmp != NULL) && (array != NULL))
 	{
 		for (i = 0; i < size; i++)
 		{
