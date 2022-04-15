522a3fa8aa21f593f75503289c103c1b59a0f94f
root <vchidubem9@gmail.com>
2022-04-14 18:35:24 -0700

    prints variable arguements

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
-
+#include "variadic_functions.h"
 #include <stdio.h>
 
 /**
@@ -15,15 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	va_start(ap, n);
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(ap, int));
-			if (i < (n - 1))
-				printf("%s", separator);
-		}
-		printf("\n");
+		printf("%d", va_arg(ap, int));
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(ap);
 }
