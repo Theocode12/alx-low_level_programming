e16d7d56e0ed36ed71a8a7b9c11001981a66cc12
root <vchidubem9@gmail.com>
2022-04-14 19:45:27 -0700

    prints the string passed as arguements

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		ptr = va_arg(ap, char *);
 		if (!ptr)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", ptr);
 		if (i < n - 1 && separator)
