45ff68d0a6c1e8fee2e59ea385b17f7559d6cfaf
root <vchidubem9@gmail.com>
2022-04-15 07:15:06 -0700

    print-all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ * print_c - prints a char
+ * @ap: arguement pointer passed
+ */
+
+void print_c(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_i - prints an int
+ * @ap: arguement pointer passed
+ */
+
+void print_i(va_list ap)
+{
+	printf("%i", va_arg(ap, int));
+}
+
+/**
+ * print_f - prints float numbers
+ * @ap: arguement passed
+ */
+
+void print_f(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_s - prints strings
+ * @ap: arguement passed
+ */
+
+void print_s(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+	if (str == NULL)
+	{
+		return;
+	}
+	printf("%s",str);
+}
+/**
+ * print_all - prints all argurments passed no matter which type
+ * @format: list of the arguement passed
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	struct var p[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+		{'\0', NULL}
+	};
+	va_list ap;
+	char *sep = "";
+
+	va_start(ap, format);
+	while (format[i])
+	{
+		j = 0;
+		while (p[j].c)
+		{
+			if (format[i] == p[j].c)
+			{
+				printf("%s", sep);
+				p[j].f(ap);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
+			
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+/**
+ * struct var - hold a char and a function pointer in order to execute the task
+ * @c: character
+ * @f: function pointer
+ */
+struct var
+{
+	char c;
+	void (*f)(va_list);
+};
+void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
