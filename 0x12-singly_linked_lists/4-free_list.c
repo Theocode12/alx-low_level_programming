fb59bffc4dcf25200ef1e09fb880f27a316d3847
Theocode12 <vchidubem9@gmail.com>
2022-04-22 05:33:45 +0100

    free allocated memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees allocated memory afer usage
+ * @head: head node
+ */
+
+void free_list(list_t *head)
+{
+      list_t *temp;
+
+      while (head)
+      {
+	      temp = head;
+	      head = head->next;
+	      free(temp->str);
+	      free(temp);
+      }
+}
