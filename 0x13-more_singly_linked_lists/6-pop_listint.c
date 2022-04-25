45927a1c85a8ec97b0363d0846a57d638a4c588b
Theocode12 <vchidubem9@gmail.com>
2022-04-25 20:04:13 +0100

    popping a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - pops a node from the list
+ * @head: pointer to the first node in the list
+ * Return: returns element that was poppped
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL)
+		return (-1);
+	temp =  *head;
+	*head = (*head)->next;
+	n = temp->n;
+	free(temp);
+	return (n);
+}
