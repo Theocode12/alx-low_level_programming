7f80013253f04c3dc3ce98aa5151748f71d48a42
Theocode12 <vchidubem9@gmail.com>
2022-04-27 05:23:22 +0100

    update on insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -46,9 +46,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = (*head);
 	if (idx == 0)
 		return (insert_node_at_head(&(*head), new));
-	if (idx == (count - 1))
+	if (idx == (count))
 	{
-		for (i = 0; i < idx; i++)
+		for (i = 0; i <= idx; i++)
 		{
 			prev_node = temp;
 			temp = temp->next;
