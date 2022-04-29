041ae33212d7f00e67e7edca44d8ebfaaac016f3
Theocode12 <vchidubem9@gmail.com>
2022-04-29 13:11:31 +0100

    get bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_bit - gets bit at a particular position
+ * @n: integer
+ * @index: position to get bit
+ * Return: bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int mask = 1 << index;
+
+	if (mask > n)
+		return (-1);
+	n = n & mask;
+	if (n > 0)
+	{
+		n = 1;
+		return (n);
+	}
+	else
+	{
+		n = 0;
+	}
+	return (n);
+}
