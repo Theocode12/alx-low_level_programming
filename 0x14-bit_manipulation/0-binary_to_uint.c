5d627b408ade7017c013655b5f4ce3e42195b121
Theocode12 <vchidubem9@gmail.com>
2022-04-29 05:31:45 +0100

    convert bin to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * binary_to_unit - convert binary to unsigned integer
+ * @b: a string of binary numbers
+ * Return: an unsigned integer
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, len, idx = 1;
+
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len]; len++)
+	{
+		if ((b[len] != '0') && (b[len] != '1'))
+		{
+			return (0);
+		}
+		if (len > 0)
+			idx *= 2;
+	}
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] == '1')
+			num += idx;
+		idx /= 2;
+	}
+	return (num);
+}
