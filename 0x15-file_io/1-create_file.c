4dba586824d790ac5ec1f73b8ba1fe4ffeaae7c0
Theocode12 <vchidubem9@gmail.com>
2022-05-03 04:19:53 +0100

    create a new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: content of the file
+ * Return: 1 if successful, 0 otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		write(STDOUT_FILENO, "fails", strlen("fails"));
+		return (-1);
+	}
+	if (!text_content)
+		close(fd);
+	else
+	{
+		write(fd, text_content, strlen(text_content));
+		close(fd);
+	}
+	return (1);
+}
