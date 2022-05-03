cf7c11c3b99d8c01ea931107df2ab5afebf67e59
Theocode12 <vchidubem9@gmail.com>
2022-05-03 02:17:45 +0100

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	sz = read(fd, arr, letters);
 	arr[letters] = '\0';
-	dprintf(STDOUT_FILENO, "%s\n", arr);
+	dprintf(STDOUT_FILENO, "%s", arr);
 	close(fd);
 	free(arr);
 	return (sz);
