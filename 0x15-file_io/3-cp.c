39fd0d92a2f825c355f7862308aa8a1d932335b5
Theocode12 <vchidubem9@gmail.com>
2022-05-03 17:03:48 +0100

    update on cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(98);
+		exit(97);
 	}
 	_cp(argv[1], argv[2]);
 	return (0);
