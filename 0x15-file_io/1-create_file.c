d2daf27824f4c5725a3410e2dc84c4970fb49e11
Theocode12 <vchidubem9@gmail.com>
2022-05-03 04:29:24 +0100

    create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
-	{
-		write(STDOUT_FILENO, "fails", strlen("fails"));
 		return (-1);
-	}
 	if (!text_content)
 		close(fd);
 	else
