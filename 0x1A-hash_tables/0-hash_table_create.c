7567e15e7c27214e0046d2d5b30ebfee3e8ea8d4
root <vchidubem9@gmail.com>
2022-06-30 12:31:20 -0700

    creating tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
 	ht->size = size;
 	ht->array = calloc(size, sizeof(unsigned long int *));
+	if (!(ht->array))
+		return (NULL);
 	return (ht);
 }
