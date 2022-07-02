9268419a5af3bd9d11961dccab64857e4c40c4c0
Valentine <vchidubem9@gmail.com>
2022-07-02 01:41:57 +0100

    delete hash tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *h_node, *nxt_node;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		h_node = ht->array[index]; 
+		while (h_node)
+		{
+			nxt_node = h_node->next;
+			free(h_node);
+			h_node = nxt_node;
+		}
+	}
+	free(ht->array);
+}
