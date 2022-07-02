44118eaf1415c8180d88d11d881f9daec8cd13f4
Valentine <vchidubem9@gmail.com>
2022-07-02 03:34:22 +0100

    sorted hash tables

---
+++ 0x1A-hash_tables/100-main.c
@@ -0,0 +1,6 @@
+#include "sorted_hash_tables.h"
+
+int main(void){
+    shash_table_create(1024);
+    return (0);
+}
---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,21 @@
+#include "sorted_hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**/
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *ht;
+
+	ht = malloc(sizeof(shash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(shash_node_t));
+	if (ht->array == NULL)
+		return (NULL);
+	ht->shead = NULL;
+	ht->stail = NULL;
+}
+
+/**/
