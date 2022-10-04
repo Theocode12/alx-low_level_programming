334ffd89d23a30f0d4ca2b32825b8401b6f86470
theocode12 <vchidubem9@gmail.com>
2022-10-04 18:44:45 +0100

    search algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
 #ifndef _SEARCH_ALGO
-#define _SERACH_ALGO
+#define _SEARCH_ALGO
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
