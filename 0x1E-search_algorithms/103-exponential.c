3aab82ba43e2dd51094a4d5e987c8a8e97febd2a
theocode12 <vchidubem9@gmail.com>
2022-10-06 00:17:04 +0100

    exp search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,76 +1,64 @@
 #include "search_algos.h"
 #include <stdio.h>
-int bin_sch(int *array, int value, int high, int lo);
 
 /**
- * min - finds minimum btw two values
- * @bound: bound
- * @size: size
- * Return: minimum
+ * print_array - prints a subset from an array
+ * @array: a pointer to the first element of the array
+ * @low: the lower boundary
+ * @high: the upper boundary
  */
-
-int min(int bound, int size)
+void print_array(int *array, size_t low, size_t high)
 {
-	if (bound < size)
-		return (bound);
-	return (size);
+	printf("Searching in array: ");
+	for (; low <= high; low++)
+	{
+		printf("%d", array[low]);
+		if (low != high)
+			printf(", ");
+	}
+	printf("\n");
 }
 
 /**
- * exponential_search - implements exponential search algo
- * @array: array to search
- * @size: size of array
- * @value: value to search
- * Return: index to which value was found else -1
+ * exponential_search - searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located if sucessful
+ * otherwise -1
  */
-
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t bound = 1, prev;
-
-	do {
-		printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
-		prev = bound;
-		bound *= 2;
-	} while (array[bound] < value && bound < size);
-
-	printf("Value found between indexes [%ld] and [%d]\n",
-			prev, min(bound, size - 1));
-	return (bin_sch(array, value, min(bound, size - 1), bound / 2));
-}
-
-/**
- * bin_sch - implementation of binary search
- * @array: array to search
- * @high: high bound of where to search
- * @lo: lower bound of where to search
- * @value: value to search
- * Return: index where value is located
- */
-
-int bin_sch(int *array, int value, int high, int lo)
-{
-	int i, mid;
+	size_t low = 0, high = 1, mid;
 
 	if (!array)
 		return (-1);
-	while (lo <= high)
+
+	do {
+		printf("Value checked array[%ld] = [%d]\n", high, array[high]);
+		if (array[high] == value)
+			return (high);
+		low = high;
+		high *= 2;
+	} while (high < size && array[high] < value);
+
+	if (high > size - 1)
+		high = size - 1;
+
+	printf("Value found between indexes [%ld] and [%ld]\n", low, high);
+
+	while (low <= high)
 	{
-		printf("Searching in array:");
-		for (i = lo; i <= high; i++)
-		{
-			if (i == high)
-				printf(" %d\n", array[i]);
-			else
-				printf(" %d,", array[i]);
-		}
-		mid = (lo + high) / 2;
-		if (array[mid] > value)
+		print_array(array, low, high);
+		mid = (low + high) / 2;
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
 			high = mid - 1;
-		else if (array[mid] < value)
-			lo = mid + 1;
 		else
 			return (mid);
 	}
+
 	return (-1);
 }
