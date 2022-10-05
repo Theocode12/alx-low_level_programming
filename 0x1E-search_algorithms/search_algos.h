bdb83a387f5fdba00f662a6457a48a0937ea8316
theocode12 <vchidubem9@gmail.com>
2022-10-05 11:54:27 +0100

    jump algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef _SEARCH_ALGO
 #define _SEARCH_ALGO
 #include <stddef.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif /*_SEARCH_ALGO*/
