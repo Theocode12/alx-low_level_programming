3f2477908729b4573f82a9be1a1aa9699db6b9a6
theocode12 <vchidubem9@gmail.com>
2022-10-05 18:17:11 +0100

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,45 @@
 #include "search_algos.h"
 #include <stdio.h>
 
+/**
+ * bs_algo - binary search algorithm
+ * @array: array to be searched
+ * @sz: size of array
+ * @value: number to search for
+ * @lst_idx: list index in arry to be searched
+ * Return: index where value is located
+ */
+
+int bs_algo(int *array, int sz, int value, int lst_idx)
+{
+	int mid_idx, i;
+
+	if (lst_idx > sz || sz < 0)
+		return (-1);
+
+	printf("Searching in array:");
+	for (i = lst_idx; i <= sz; i++)
+	{
+		if (i == sz)
+			printf(" %d\n", array[i]);
+		else
+			printf(" %d,", array[i]);
+	}
+
+	mid_idx = (sz + lst_idx) / 2;
+	if (array[mid_idx] > value)
+	{
+		return (bs_algo(array, mid_idx - 1, value, lst_idx));
+	}
+
+	else if (array[mid_idx] < value)
+	{
+		lst_idx = mid_idx + 1;
+		return (bs_algo(array, sz, value, lst_idx));
+	}
+
+	return (mid_idx);
+}
 
 /**
  * binary_search - implementation of binary search
@@ -12,29 +51,9 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t lo, high, i, mid;
+	int lst_idx = 0;
 
 	if (!array)
 		return (-1);
-	lo = 0;
-	high = size - 1;
-	while (lo <= high)
-	{
-		printf("Searching in array:");
-		for (i = lo; i <= high; i++)
-		{
-			if (i == high)
-				printf(" %d\n", array[i]);
-			else
-				printf(" %d,", array[i]);
-		}
-		mid = (lo + high) / 2;
-		if (array[mid] > value)
-			high = mid - 1;
-		else if (array[mid] < value)
-			lo = mid + 1;
-		else
-			return (mid);
-	}
-	return (-1);
+	return (bs_algo(array, size - 1, (int)value, lst_idx));
 }
