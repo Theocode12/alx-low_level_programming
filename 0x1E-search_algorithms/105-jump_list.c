3c90a5bb9ba4f65e66b5112cbb039833865fda9a
theocode12 <vchidubem9@gmail.com>
2022-10-06 14:57:43 +0100

    jump search algo

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * jump_list - implements jump-search algo an linked list
+ * @list: head of the list to search
+ * @size: number of nodes in linked list
+ * @value: value to search for
+ * Return: node where value is found else NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step;
+	listint_t *prev = list;
+
+	if (!list)
+		return (NULL);
+	step = sqrt(size);
+	while (list->next)
+	{
+		if (list->index == step)
+		{
+			printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+			if (list->n <= value)
+				prev = list;
+			else
+				break;
+			step += sqrt(size);
+		}
+		list = list->next;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+						prev->index, list->index);
+
+	while (prev != list->next)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
+	return (NULL);
+}
